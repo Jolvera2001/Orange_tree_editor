3f1c41563d39d0999fae877fbf3001a3131702bc
Jolvera2001 <jolvera6@stedwards.edu>
2025-06-20 09:30:48 -0500

    Applied editor colors

--- Orange_tree_editor/Views/MainWindow.axaml.cs
+++ Orange_tree_editor/Views/MainWindow.axaml.cs
@@ -1,10 +1,8 @@
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.Media;
 using AvaloniaEdit;
-using AvaloniaEdit.Highlighting;
 using AvaloniaEdit.TextMate;
-using Markdown.Avalonia.SyntaxHigh;
-using Microsoft.CodeAnalysis;
 using Orange_tree_editor.ViewModels;
 using TextMateSharp.Grammars;
 
@@ -24,16 +22,56 @@ public partial class MainWindow : Window
         Editor.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
         Editor.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
         SetupHighlighting(Editor);
-        
     }
 
-    public void SetupHighlighting(TextEditor editor)
+    private void SetupHighlighting(TextEditor editor)
     {
-        var registryOptions = new RegistryOptions(ThemeName.Monokai);
+        var registryOptions = new RegistryOptions(ThemeName.DarkPlus);
         var textMateInstallation = editor.InstallTextMate(registryOptions);
+        
+        textMateInstallation.AppliedTheme += (sender, installation) =>
+        {
+            ApplyThemeColors(editor, installation);
+        };
         
         var language = registryOptions.GetLanguageByExtension(".md");
         var scopeName = registryOptions.GetScopeByLanguageId(language.Id);
         textMateInstallation.SetGrammar(scopeName);
+        ApplyThemeColors(Editor, textMateInstallation);
+    }
+
+    private void ApplyThemeColors(TextEditor editor, TextMate.Installation installation)
+    {
+        if (installation.TryGetThemeColor("editor.background", out var bgColor))
+        {
+            if (Color.TryParse(bgColor, out var color))
+            {
+                editor.Background = new SolidColorBrush(color);
+            }
+        }
+
+        if (installation.TryGetThemeColor("editor.foreground", out var fgColor))
+        {
+            if (Color.TryParse(fgColor, out var color))
+            {
+                editor.Foreground = new SolidColorBrush(color);
+            }
+        }
+        
+        if (installation.TryGetThemeColor("editor.selectionBackground", out var selColor))
+        {
+            if (Color.TryParse(selColor, out var color))
+            {
+                editor.TextArea.SelectionBrush = new SolidColorBrush(color);
+            }
+        }
+        
+        if (installation.TryGetThemeColor("editorLineNumber.foreground", out var lineNumColor))
+        {
+            if (Color.TryParse(lineNumColor, out var color))
+            {
+                editor.LineNumbersForeground = new SolidColorBrush(color);
+            }
+        }
     }
 }
